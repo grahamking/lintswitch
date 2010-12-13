f99e4b79de5d1eef611ab662f56d5f86331aa509
Graham King <graham@gkgk.org>
2010-12-13 11:54:26 -0800

    Install script seems to work. Updated README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Summary
 
-*lint_switch* is a small Bash script that connects:
+*lintswitch* is a small Bash script that connects:
 
 1. _incron_ which watches your files
 1. _pylint_, _jslint_ and other linters, which warn you about possible problems
@@ -12,89 +12,19 @@ The result is that your code is constantly being watched and linted,
 in the background, without interrupting your workflow, unless
 there is an error in your code.
 
+Linters and file types currently supported:
+
+ - Python: pylint, pep8
+ - Javascript: jslint, gjslint (Google Closure's Linter)
+ - CSS: jslint (yes jslint lint's CSS too)
+
+lintswitch requires Gnome, and has only been tested on Ubuntu.
+
 # Installation
 
-I have only tested lint_switch on Ubuntu. It should work on other Unix-based operating systems, as long as you can install the requirements.
-
-## Install the requirements
-
-    sudo apt-get install libnotify-bin incron zenity pylint pep8 rhino gconf2 imagemagick subversion git-core
-
-You will probably already have some of these.
-
- - libnotify-bin gives us _notify-send_
- - rhino is needed to run _jslint_
- - imagemagick converts text to an image and gconf2 sets it as the desktop background
-
-**To get Google Closure Lint:**
-
-    cd <temp_dir>
-    svn checkout http://closure-linter.googlecode.com/svn/trunk/ closure-linter
-    cd closure-linter
-    sudo python setup.py install
-
-**To get jslint:**
-
-The original jslint only runs on a webpage. There are a variety of wrappers to get it running in other environments. 
-I am using (and I recommend) jslint4java, a Java wrapper which uses the Rhino javascript engine, allowing us to run jslint from a unix command line.
-
-    cd <temp_dir>
-    wget http://jslint4java.googlecode.com/files/jslint4java-1.4.4-dist.zip
-    unzip jslint4java-1.4.4-dist.zip 
-    cd jslint4java-1.4.4
-    cp jslint4java-1.4.4.jar /usr/local/lib/
-
-## Check out / clone lint_switch
-
     git clone git://github.com/grahamking/lint_switch.git
-
-I like to symlink it from my bin directory
-
-    cd ~/bin
-    ln -s [your checkout]/lintswitch
-
-Open _lint_switch.sh_ and edit the JSLINT_DIR to point to where jslint4java-1.4.4.jar lives.
-
-For example my JSLINT_DIR is _/home/graham/Applications_
-
-## Setup incron
-
-Add your username to /etc/incron.allow:
-
-    sudo bash -c "echo $(whoami) >> /etc/incron.allow"
-
-Next, tell incron what to watch. 
-
-First generate a list of relevant files. If you know the incron format, you can enter _incrontab -e_  and edit by hand. Otherwise, here's a command to help you:
-
-    PROJ_PATH=[proj_path] ; LS=[lint_switch_location] ; find $PROJ_PATH -name "*.py" -or -name "*.js" -or -name "*.css" | xargs -l1 dirname | sort | uniq | awk "{print \$1 \" IN_ATTRIB $LS \$@/\$# $PROJ_PATH\"}"
-
-
-In the command above you need to replace two things:
- - [proj_path] Replace this with the full path to your project. For example for a Django project I worked on recently I used _PROJ_PATH=/home/graham/Projects/fablistic/fabproject_. That is also the directory the linter will run from.  
- - [lint_switch_location] Replace with the path and name for lint_switch.sh. For example I use _LS=/home/graham/bin/lint_switch.sh_
-
-This will find all directories which contain 'py', 'js', or 'css' files. Customise as required.
-
-Run incrontab and paste the result into it:
-
-    incrontab -e
-
-Do this for each project you want to watch and lint.
-
-## Set screen resolution in lint_switch.sh
-
-Edit lint_switch.sh and change the SCREEN_X and SCREEN_Y variables to be about 50 pixels less than your screen's resolution. Go to System / Preferences / Monitors to find out.
-
-While you're there take a look at the other configuration variables, see if you fancy tweaking any.
-
-## Make sure your editor doesn't move the file
-
-Vim, by default, when you save a file, moves it to create a backup, and creates a new file. This means inotify (incron) is watching the old file.
-
-Add this to your .vimrc:
-
-    set nowritebackup 
+    cd lint_switch
+    ./install.sh
 
 ## Test it
 
@@ -106,9 +36,13 @@ You should see:
 directly on your desktop (root window).
 - If lint is happy: Just a notification. Good job.
 
-## Customise it
+# Misc.
 
-All this script does is wire together a number of other tools. It's just bash script, and there are lots of comments. Hopefully you can customise it for your particular situation.
+## Configure and Customise
+
+The config file is `/usr/local/etc/lintswitch.conf` - syntax is bash script 
+
+All lintswitch does does is wire together a number of other tools. It's just bash script, and there are lots of comments. Hopefully you can customise it for your particular situation.
 
 ## Contribute
 
--- install.sh
+++ install.sh
@@ -7,49 +7,88 @@
 
 # Installs the packages lintswitch relies upon
 install_dependencies() {
+    echo ""
+    echo "* Using apt-get to install dependencies"
+    echo ""
     sudo apt-get install libnotify-bin incron zenity pylint pep8 rhino gconf2 imagemagick subversion git-core
 }
 
 # Install Google's closure linter
 install_closure() {
-    cd /tmp
-    svn checkout http://closure-linter.googlecode.com/svn/trunk/ closure-linter
-    cd closure-linter
-    sudo python setup.py install
-    cd -
+
+    local ignore=$(which gjslint)
+    if [ $? -eq 1 ]
+    then
+        echo ""
+        echo "* Installing Google's Closure Linter"
+        echo ""
+
+        cd /tmp
+        svn checkout http://closure-linter.googlecode.com/svn/trunk/ closure-linter
+        cd closure-linter
+        sudo python setup.py install
+        cd $START_DIR 
+    else
+        echo "Google Closure Linter already installed"
+    fi
 }
 
 install_jslint4java() {
-    cd /tmp
-    wget http://jslint4java.googlecode.com/files/jslint4java-1.4.4-dist.zip
-    unzip jslint4java-1.4.4-dist.zip 
-    cd jslint4java-1.4.4
-    sudo cp jslint4java-1.4.4.jar /usr/local/lib/
-    cd -
+    if [ ! -f /usr/local/lib/jslint4java-1.4.4 ]
+    then
+        echo ""
+        echo "* Installing jslint4java (command-line version of jslint)"
+        echo ""
+        cd /tmp
+        wget http://jslint4java.googlecode.com/files/jslint4java-1.4.4-dist.zip
+        unzip jslint4java-1.4.4-dist.zip 
+        cd jslint4java-1.4.4
+        sudo cp jslint4java-1.4.4.jar /usr/local/lib/
+        cd $START_DIR
+    else
+        echo "jslint4java already installed"
+    fi
 }
 
 install_lintswitch() {
-    sudo cp lintswitch.sh /usr/local/bin/
-    sudo cp lintswitch.conf /usr/local/etc/
+    if [ ! -f /usr/local/bin/lintswitch.sh ]
+    then
+        echo ""
+        echo "* Copying lintswitch to /usr/local/bin and lintswitch.conf to /usr/local/etc"
+        echo ""
+
+        sudo cp lintswitch.sh /usr/local/bin/
+        sudo cp lintswitch.conf /usr/local/etc/
+    else
+        echo "lintswitch files already installed"
+    fi
 }
 
 # Incron needs your name in /etc/incron.allow before you are allowed to use it
 allow_me_to_use_incron() {
+
     local me=$(whoami)
     local already_done=$(sudo cat /etc/incron.allow | grep $me | wc -l)
-    if [ -n "$already_done" ]
+    if [ "$already_done" -eq 0 ]
     then
+        echo ""
+        echo "* Adding your username to /etc/incron.allow, so that you can use incron"
+        echo ""
         sudo bash -c "echo $me >> /etc/incron.allow"
+    else
+        echo "You are already permissioned for incron"
     fi
 }
 
 add_code_dirs() {
+    echo ""
     echo "You now need to pick directories to watch and lint."
     echo ""
-    echo "In the dialog that follows, please use Ctrl to pick all relevant top level project directories."
+    echo "In the dialog that follows, hold down Ctrl to pick all relevant top level project directories."
     echo "These should be the directory that goes on your PYTHONPATH. For example in a Django project select your 'project' directory (not your app directories, and not the directory above your project)".
     echo ""
     echo "If you don't get it right, simply run install again. Or type 'incrontab -e' to edit manually"
+    echo ""
     echo ""
     read -n1 -r -p "Press any key to continue..."
 
@@ -62,28 +101,52 @@ add_code_dirs() {
 
     for proj_path in $arr
     do
-        incron_lines=$(find $proj_path -name "*.py" -or -name "*.js" -or -name "*.css" | xargs -l1 dirname | sort | uniq | awk "{print \$1 \" IN_ATTRIB /usr/local/bin/lintswitch.sh \$@/\$# $proj_path \"}")
+        incron_lines=$(find $proj_path -name "*.py" -or -name "*.js" -or -name "*.css" | xargs -l1 dirname | sort | uniq | awk "{print \$1 \" IN_ATTRIB /usr/local/bin/lintswitch.sh PATH_VALUES $proj_path \"}")
         sudo bash -c "echo \"$incron_lines\" >> /var/spool/incron/${me}"
+        sudo sed --in-place 's/PATH_VALUES/$@\/$#/' /var/spool/incron/${me}
     done
 }
 
 set_screen_xy() {
+    echo ""
+    echo "* Setting screen dimensions"
+    echo ""
+
     local dim=$(xdpyinfo | grep dimensions | awk '{print $2}')
-    echo $dim | awk -F x '{print $1}'
-    echo $dim | awk -F x '{print $2}'
+    local screenx=$(echo $dim | awk -F x '{print $1}')
+    local screeny=$(echo $dim | awk -F x '{print $2}')
+    if [ "$screenx" -gt 2000 ]
+    then
+        local num_screens=$(zenity --list --radiolist --text="How many screens do you have?" --column="Select" --column="Number of screens" TRUE 1 FALSE 2 FALSE 3 FALSE 4)
+        screenx=$((screenx/num_screens))
+    fi
+    screenx=$((screenx-100))
+    screeny=$((screeny-100))
+
+    sudo sed --in-place "s/SCREEN_X_GOES_HERE/$screenx/" /usr/local/etc/lintswitch.conf
+    sudo sed --in-place "s/SCREEN_Y_GOES_HERE/$screeny/" /usr/local/etc/lintswitch.conf
 }
 
-#if [ "$(whoami)" != "root" ]
-#then
-#    echo "Install must be run as root, because it needs to write to /usr/local/bin and /etc"
-#    exit 1
-#fi
+prompt_editor_move_file() {
+    echo ""
+    echo "* Final step: Check your editor does not move / rename the file you're editing"
+    echo "* Vim users will need this in their .vimrc:"
+    echo "*   set nobackup"
+    echo "*   set nowritebackup"
+    echo ""
+    echo "lintswitch INSTALL COMPLETED"
+}
 
-install_dependencies
-install_closure
-install_jslint4java
-install_lintswitch
-allow_me_to_use_incron
-add_code_dirs
-set_screen_xy
+main() {
+    START_DIR=$(pwd)
+    install_dependencies
+    install_closure
+    install_jslint4java
+    install_lintswitch
+    allow_me_to_use_incron
+    add_code_dirs
+    set_screen_xy
+    prompt_editor_move_file
+}
 
+#main
